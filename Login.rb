c6edcd6963377272c4f865a2216146a2311b6357
stlireri@gmail.com <stellaireri@gmail.com>
2018-03-20 18:36:28 +0300

    My first testing framework

---
+++ Login.rb
@@ -0,0 +1,33 @@
+
+require "selenium-webdriver"
+
+#Firefox browser instantiation
+driver = Selenium::WebDriver.for :chrome
+
+#Loading the assertselenium URL
+driver.navigate.to "http://nm.m-kopa.net"
+=begin
+#Clicking on the Follow link present on the assertselenium home page
+FollowButton  = driver.find_element(:link, "Follow")
+FollowButton.click
+
+#Typing the UserName
+LoginButton = driver.find_element(:id, "user_login")
+LoginButton.send_keys "sampleuser77dff27"
+
+#Typing the Email-Id
+EmailId = driver.find_element(:id, "user_email")
+EmailId.send_keys "sampleuser7f7df27@gmail.com"
+
+#Clicking on the Submit Button
+SubmitButton = driver.find_element(:id, "wp-submit")
+SubmitButton.click
+
+#Asserting whether the registration success message is diaplyed
+SuccessMessage = driver.find_element(:css, "p.message")
+"Registration complete. Please check your e-mail.".eql? SuccessMessage.text
+puts "Successfully completed the user registration and validated the Success message"
+=end
+#Quitting the browser
+driver.quit
+
