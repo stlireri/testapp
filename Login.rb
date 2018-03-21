329ec84e87e895884c5b055725174d664507cc31
stlireri@gmail.com <stellaireri@gmail.com>
2018-03-22 00:32:09 +0300

    fill in the login form

--- Login.rb
+++ Login.rb
@@ -6,28 +6,29 @@ driver = Selenium::WebDriver.for :chrome
 
 #Loading the assertselenium URL
 driver.navigate.to "http://nm.m-kopa.net"
-=begin
+
 #Clicking on the Follow link present on the assertselenium home page
-FollowButton  = driver.find_element(:link, "Follow")
-FollowButton.click
+#FollowButton  = driver.find_element(:link, "Follow")
+#FollowButton.click
+
 
 #Typing the UserName
-LoginButton = driver.find_element(:id, "user_login")
+LoginButton = driver.find_element(:id, "UserNameOrEmail")
 LoginButton.send_keys "sampleuser77dff27"
 
 #Typing the Email-Id
-EmailId = driver.find_element(:id, "user_email")
+EmailId = driver.find_element(:id, "Password")
 EmailId.send_keys "sampleuser7f7df27@gmail.com"
 
 #Clicking on the Submit Button
-SubmitButton = driver.find_element(:id, "wp-submit")
-SubmitButton.click
+SubmitButton = driver.find_elements(:name, "Sign In")
+SubmitButton.Click
 
 #Asserting whether the registration success message is diaplyed
-SuccessMessage = driver.find_element(:css, "p.message")
-"Registration complete. Please check your e-mail.".eql? SuccessMessage.text
-puts "Successfully completed the user registration and validated the Success message"
-=end
+SuccessMessage = driver.find_element(:css, "div#divLoginFormSub > form > div:nth-of-type(2) > div > ul > li:nth-of-type(2)")
+"invalid email or password".eql? SuccessMessage.text
+#puts "Successfully completed the user registration and validated the Success message"
+
 #Quitting the browser
 driver.quit
 
